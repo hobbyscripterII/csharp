355a1ccbd8dc45c48223c0c45f5dc467ae4c0a55
이주영 <wruomma2@naver.com>
2024-03-17 01:12:56 +0900

    windows form 테스트(기계 시작/정지 버튼)

---
+++ ContaminationZone/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace ContaminationZone
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Msg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Chartreuse;
+            this.Start.Location = new System.Drawing.Point(12, 12);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(191, 84);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "시작";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(209, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "정지";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Msg
+            // 
+            this.Msg.AutoSize = true;
+            this.Msg.Location = new System.Drawing.Point(12, 117);
+            this.Msg.Name = "Msg";
+            this.Msg.Size = new System.Drawing.Size(9, 12);
+            this.Msg.TabIndex = 2;
+            this.Msg.Text = " ";
+            this.Msg.Click += new System.EventHandler(this.Msg_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 147);
+            this.Controls.Add(this.Msg);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Msg;
+    }
+}
+
---
+++ ContaminationZone/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContaminationZone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Msg.Text = "기계를 가동합니다.";
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Msg.Text = "기계를 정지합니다.";
+        }
+
+        private void Msg_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
