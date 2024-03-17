b168096cc5d53c4f15177a3f25c5ded1d538b8c0
이주영 <wruomma2@naver.com>
2024-03-17 22:59:59 +0900

    thread를 이용한 양품 판별기

---
+++ Exception/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_
+{
+    internal class Program
+    {
+        private const int MAX_VALUE = 1024;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int i = 2000;
+
+                if(i > MAX_VALUE)
+                {
+                    throw new Exception();
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine("예외가 발생하였습니다.");
+                Console.WriteLine(e);
+                Console.ReadKey();
+                throw e;
+            }
+        }
+    }
+}
---
+++ YieldFormsTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YieldFormsTest
+{
+    internal class Program
+    {
+        private const int MAX_VALUE = 100;
+
+        void Run()
+        {
+            Random random = new Random(); // Random 네임스페이스를 통해 객체 생성
+            int passed = 0; // 양품
+            int failed = 0; // 불량
+
+            for (int i = 0; i <= MAX_VALUE; i++)
+            {
+                int randomVar = random.Next(1, 10);
+                bool result = randomVar <= 8 ? true : false;
+
+                if (result)
+                {
+                    passed++;
+                }
+                else
+                {
+                    failed++;
+                }
+                Console.WriteLine("random = " + randomVar);
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("passed cnt = " + passed);
+            Console.WriteLine("failed cnt = " + failed);
+            Console.ReadKey();
+        }
+
+        void doTest()
+        {
+            Thread thread = new Thread(new ThreadStart(Run));
+            thread.Start();
+        }
+
+        static void Main(string[] args)
+        {
+            new Program().doTest();
+        }
+    }
+}
